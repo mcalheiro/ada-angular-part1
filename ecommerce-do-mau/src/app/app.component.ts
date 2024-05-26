862bf19f951d4eb18c5f78e8d9a46de99a718d48
Mauricio Calheiro <mauriciocalheiro95@gmail.com>
2024-05-26 22:32:31 +0100

    Add new header component

--- ecommerce-do-mau/src/app/app.component.ts
+++ ecommerce-do-mau/src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatToolbarModule],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

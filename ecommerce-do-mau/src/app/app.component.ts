26b8609794a3a82360709d922a60f335c3e6c3ca
Mauricio Calheiro <mauriciocalheiro95@gmail.com>
2024-05-26 21:18:32 +0100

    Add type to title

--- ecommerce-do-mau/src/app/app.component.ts
+++ ecommerce-do-mau/src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'ecommerce-do-mau';
+  title: String = 'ecommerce-do-mau';
 }
